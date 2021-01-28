1464981ca9fdd09d1a2f897009fe875d41aacc0d
Michel Wehrli <wehrli92@gmail.com>
2021-01-28 21:28:38 +0100

    catch all exceptions

--- src/main.ts
+++ src/main.ts
@@ -7,46 +7,60 @@ import * as fs from 'fs'
 import helmet from 'helmet'
 import * as https from 'https'
 
+import Logger from './service/Logger'
 import Router from './service/Router'
 
-dotenv.config({
-  path: '../.env',
-})
+try {
+  dotenv.config({
+    path: '../.env',
+  })
 
-const app = express()
-app.use(helmet())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
-app.use(cookieParser())
-app.use(
-  bodyParser.json({
-    limit: '100mb',
-  })
-)
-app.use(
-  cors({
-    origin: 'https://new-crm.datapot.ch',
-    credentials: true,
-  })
-)
+  const app = express()
+  app.use(helmet())
+  app.use(
+    bodyParser.urlencoded({
+      extended: true,
+    })
+  )
+  app.use(cookieParser())
+  app.use(
+    bodyParser.json({
+      limit: '100mb',
+    })
+  )
+  app.use(
+    cors({
+      origin: 'https://new-crm.datapot.ch',
+      credentials: true,
+    })
+  )
 
-Router.listen(app).then(() => {
-  https
-    .createServer(
-      {
-        key: fs.readFileSync(
-          process.env[`DATAPOT_SSL_KEY_LOCATION_${process.env.DATAPOT_MODE}`],
-          'utf-8'
-        ),
-        cert: fs.readFileSync(
-          process.env[`DATAPOT_SSL_CERT_LOCATION_${process.env.DATAPOT_MODE}`],
-          'utf-8'
-        ),
-      },
-      app
-    )
-    .listen(process.env.DATAPOT_PORT)
-})
+  Router.listen(app).then(() => {
+    https
+      .createServer(
+        {
+          key: fs.readFileSync(
+            process.env[`DATAPOT_SSL_KEY_LOCATION_${process.env.DATAPOT_MODE}`],
+            'utf-8'
+          ),
+          cert: fs.readFileSync(
+            process.env[
+              `DATAPOT_SSL_CERT_LOCATION_${process.env.DATAPOT_MODE}`
+            ],
+            'utf-8'
+          ),
+        },
+        app
+      )
+      .listen(process.env.DATAPOT_PORT)
+  })
+} catch (exc) {
+  Logger.log(
+    'GLOBAL',
+    `${new Date().toLocaleTimeString('de-CH', {
+      year: 'numeric',
+      month: '2-digit',
+      day: '2-digit',
+    })}\t\tERROR -> ${exc.message}`
+  )
+}
